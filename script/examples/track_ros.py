654a6949d16e691c12730f2103dfc0617339daef
Jhih Yang <jerry0610804.eed06@nctu.edu.tw>
2023-10-29 22:22:23 +0800

    yolo_track_new

---
+++ script/examples/track_ros.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Author: Jerry
+# Date: Oct, 29, 2023
+
+# ROS related imports
+import rospy
+from std_msgs.msg import String, Header
+from sensor_msgs.msg import image
+from cv_bridge import CvBridgr, CvBridgeError
+from vision_msgs.msg import Detection2D, Detection2DArray, ObjectHypothesisWithPose
+import roslib
+
+# system related imports
+import argparse
+from functools import partial
+from pathlib import Path
+
+# pytorch & boxmot
+from boxmot import TRACKERS
+from boxmot.tracker_zoo import create_tracker
+from boxmot.utils import ROOT, WEIGHTS
+from boxmot.utils.checks import TestRequirements
+from examples.detectors import get_yolo_inferer
+
+__tr = TestRequirements()
+__tr.check_packages(('ultralytics @ git+https://github.com/mikel-brostrom/ultralytics.git', ))  # install
+
+
+
+import math
+import sys
+import cv2
+
+
